00b3c1ef85ffaccb2595c11241540bbf5fa7a988
Simão Gato <simao.valedegato@gmail.com>
2023-11-10 18:48:54 +0000

    refactor file names

---
+++ 2015/day1/challenge1.rs
@@ -0,0 +1,13 @@
+use std::fs;
+
+fn main() {
+    // Read input from a file
+    let input = fs::read_to_string("./input.txt").expect("Failed to read file");
+
+    // Count the number of '(' and ')' in input using iterators
+    let up = input.chars().filter(|&c| c == '(').count();
+    let down = input.chars().filter(|&c| c == ')').count();
+
+    // Print the difference between up and down
+    println!("{}", up - down);
+}
