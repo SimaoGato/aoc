7bd824bdd58bfb9510e0543195e4757affae304e
Simão Gato <simao.valedegato@gmail.com>
2023-12-01 14:52:11 +0000

    do challenge 2 day01 aoc2023

--- 2023/day01.go
+++ 2023/day01.go
@@ -5,19 +5,88 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	filePath := "inputs/day01.in"
-	totalCalibration, err := challenge1(filePath)
+	totalCalibration, err := calculateTotalCalibration(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	totalImprovedCalibration, err := calculateTotalImprovedCalibration(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Printf("Total Calibration: %d\n", totalCalibration)
+	fmt.Printf("Total Improved Calibration: %d\n", totalImprovedCalibration)
 }
 
-func challenge1(filePath string) (int, error) {
+func calculateTotalImprovedCalibration(filePath string) (int, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	var totalCalibration int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		calibration, err := calculateImprovedCalibration(line)
+		if err != nil {
+			return 0, err
+		}
+
+		totalCalibration += calibration
+	}
+
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+
+	return totalCalibration, nil
+}
+
+func calculateImprovedCalibration(line string) (int, error) {
+	line = replaceNumbersWithDigits(line)
+
+	calibration, _ := calculateCalibration(line)
+
+	return calibration, nil
+}
+
+func replaceNumbersWithDigits(input string) string {
+	replacements := map[string]string{
+		"one":   "o1e",
+		"two":   "t2o",
+		"three": "t3e",
+		"four":  "4",
+		"five":  "5e",
+		"six":   "6",
+		"seven": "7n",
+		"eight": "e8t",
+		"nine":  "n9e",
+	}
+
+	for i := 0; i < len(input); i++ {
+		if input[i] >= 'a' && input[i] <= 'z' {
+			for key, value := range replacements {
+				if strings.HasPrefix(input[i:], key) {
+					input = input[:i] + value + input[i+len(key):]
+				}
+			}
+		}
+	}
+
+	return input
+}
+
+func calculateTotalCalibration(filePath string) (int, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return 0, err
@@ -35,7 +104,6 @@ func challenge1(filePath string) (int, error) {
 			return 0, err
 		}
 
-		fmt.Printf("Calibration for line \"%s\": %d\n", line, calibration)
 		totalCalibration += calibration
 	}
 
