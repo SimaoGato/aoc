9510510c492535f47d3286142fd5d2a848fe94d1
Simão Gato <simao.valedegato@gmail.com>
2023-12-01 11:48:45 +0000

    refactor challenge 1 day01 aoc2023

--- 2023/day01.go
+++ 2023/day01.go
@@ -8,9 +8,19 @@ import (
 )
 
 func main() {
-	file, err := os.Open("inputs/day01.in")
+	filePath := "inputs/day01.in"
+	totalCalibration, err := challenge1(filePath)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	fmt.Printf("Total Calibration: %d\n", totalCalibration)
+}
+
+func challenge1(filePath string) (int, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return 0, err
 	}
 	defer file.Close()
 
@@ -20,45 +30,38 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		// Calculate the calibration value for each line
-		calibration := 0
-		found_first_digit := false
-		found_last_digit := false
-		first_digit := 0
-		last_digit := 0
-
-		for i := 0; i < len(line); i++ {
-
-			// Check if line[i] is a digit
-			if line[i] >= '0' && line[i] <= '9' && !found_first_digit {
-				// Convert line[i] to int
-				first_digit = int(line[i] - '0')
-				found_first_digit = true
-			}
-
-			// Check if line[len(line)-i] is a digit
-			if line[len(line)-i-1] >= '0' && line[len(line)-i-1] <= '9' && !found_last_digit {
-				// Convert line[len(line)-i] to int
-				last_digit = int(line[len(line)-i-1] - '0')
-				found_last_digit = true
-			}
-
-			// If both digits are found, break out of the loop
-			if found_first_digit && found_last_digit {
-				break
-			}
-
+		calibration, err := calculateCalibration(line)
+		if err != nil {
+			return 0, err
 		}
-		
-		calibration = first_digit * 10 + last_digit
 
 		fmt.Printf("Calibration for line \"%s\": %d\n", line, calibration)
 		totalCalibration += calibration
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
 
-	fmt.Printf("Total Calibration: %d\n", totalCalibration)
+	return totalCalibration, nil
+}
+
+func calculateCalibration(line string) (int, error) {
+	var firstDigit, lastDigit int
+
+	for i := 0; i < len(line); i++ {
+		if line[i] >= '0' && line[i] <= '9' {
+			firstDigit = int(line[i] - '0')
+			break
+		}
+	}
+
+	for i := len(line) - 1; i >= 0; i-- {
+		if line[i] >= '0' && line[i] <= '9' {
+			lastDigit = int(line[i] - '0')
+			break
+		}
+	}
+
+	return firstDigit*10 + lastDigit, nil
 }
