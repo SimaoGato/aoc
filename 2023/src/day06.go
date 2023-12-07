1300919cef961abb022dda4bd2ed8439d54dafc9
Simão Gato <simao.valedegato@gmail.com>
2023-12-07 17:20:41 +0000

    do challenge 02 day 06 aoc 2023

--- 2023/src/day06.go
+++ 2023/src/day06.go
@@ -20,23 +20,69 @@ func main() {
 	// Create a scanner to read the file line by line
 	scanner := bufio.NewScanner(file)
 
-	// Initialize a map to store CorrelatedData instances
-	dataMap := make(map[int]int)
-
-	// Parse input to dateMap
-	parseInputToDataMap(scanner, &dataMap)
-
-	winWays := calculateWinWays(dataMap)
-
-	fmt.Println("Winning ways multiplied together: ", winWays)
-}
-
-func parseInputToDataMap(scanner *bufio.Scanner, dataMap *map[int]int) {
 	scanner.Scan()
 	timeLine := scanner.Text()
 	scanner.Scan()
 	distanceLine := scanner.Text()
 
+	// Initialize a map to store CorrelatedData instances
+	dataMap := make(map[int]int)
+
+	// Parse input to dateMap
+	parseInputToDataMap(&dataMap, timeLine, distanceLine)
+
+	winWays := calculateWinWays(dataMap)
+
+	fmt.Println("Winning ways multiplied together are: ", winWays)
+
+	// Parse input the correct way
+	correctTime, correctDistance := parseInputToCorrectData(timeLine, distanceLine)
+
+	winWays = calculateWinWaysCorrect(correctTime, correctDistance)
+
+	fmt.Println("Winning ways multiplied together, the correct way, are: ", winWays)
+}
+
+func parseInputToCorrectData(timeLine string, distanceLine string) (int, int) {
+
+	correctTime := 0
+	correctDistance := 0
+
+	for i := 0; i < len(timeLine); i++ {
+		if timeLine[i] >= '0' && timeLine[i] <= '9' {
+			correctTime = correctTime*10 + int(timeLine[i]-'0')
+		}
+	}
+
+	for i := 0; i < len(distanceLine); i++ {
+		if distanceLine[i] >= '0' && distanceLine[i] <= '9' {
+			correctDistance = correctDistance*10 + int(distanceLine[i]-'0')
+		}
+	}
+
+	return correctTime, correctDistance
+}
+
+func calculateWinWaysCorrect(correctTime int, correctDistance int) int {
+	var winWays int = 1
+
+	countWinWays := 0
+
+	for holdButton := 0; holdButton <= correctTime; holdButton++ {
+		timeLeft := correctTime - holdButton
+		velocity := holdButton
+
+		if timeLeft*velocity > correctDistance {
+			countWinWays++
+		}
+	}
+
+	winWays *= countWinWays
+
+	return winWays
+}
+
+func parseInputToDataMap(dataMap *map[int]int, timeLine string, distanceLine string) {
 	times := strings.Fields(timeLine)
 
 	distances := strings.Fields(distanceLine)
