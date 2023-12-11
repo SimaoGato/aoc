50647baa5e70c40e4d8f090ab0472120333feaaf
Simão Gato <simao.valedegato@gmail.com>
2023-12-11 11:22:56 +0000

    do challenge 02 day 07 aoc 2023

--- 2023/src/day07.go
+++ 2023/src/day07.go
@@ -34,6 +34,22 @@ var CardHits = map[string]int{
 	"2": 1, // 2 has the lowest value
 }
 
+var CardHitsWithJoker = map[string]int{
+	"A": 13, // A has the highest value
+	"K": 12,
+	"Q": 11,
+	"T": 10,
+	"9": 9,
+	"8": 8,
+	"7": 7,
+	"6": 6,
+	"5": 5,
+	"4": 4,
+	"3": 3,
+	"2": 2,
+	"J": 1, // 2 has the lowest value
+}
+
 func main() {
 	filePath := "../inputs/day07.in"
 
@@ -46,6 +62,8 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
+	// Part 1
+
 	hands, bets := parseGameInput(scanner)
 
 	bubbleSort(hands, bets)
@@ -53,6 +71,14 @@ func main() {
 	totalWinnings := calculateTotalWinnings(bets)
 
 	fmt.Println("Total winnings are: ", totalWinnings)
+
+	// Part 2
+
+	bubbleSortWithJoker(hands, bets)
+
+	totalWinningsWithJoker := calculateTotalWinnings(bets)
+
+	fmt.Println("Total winnings with joker are: ", totalWinningsWithJoker)
 }
 
 func parseGameInput(scanner *bufio.Scanner) ([]string, []int) {
@@ -80,6 +106,208 @@ func bubbleSort(hands []string, bets []int) {
 			}
 		}
 	}
+}
+
+func bubbleSortWithJoker(hands []string, bets []int) {
+	for i := 0; i < len(hands)-1; i++ {
+		for j := 0; j < len(hands)-i-1; j++ {
+			if isHand1BetterThanHand2WithJoker(hands[j], hands[j+1]) {
+				bets[j], bets[j+1] = bets[j+1], bets[j]
+				hands[j], hands[j+1] = hands[j+1], hands[j]
+			}
+		}
+	}
+}
+
+func isHand1BetterThanHand2WithJoker(hand1 string, hand2 string) bool {
+	hand1Type := getHandTypeWithJoker(hand1)
+	hand2Type := getHandTypeWithJoker(hand2)
+
+	if hand1Type > hand2Type {
+		return true
+	} else if hand1Type < hand2Type {
+		return false
+	} else {
+		return isHand1BetterThanHand2AuxWithJoker(hand1, hand2)
+	}
+}
+
+func isHand1BetterThanHand2AuxWithJoker(hand1 string, hand2 string) bool {
+	for i := 0; i < 5; i++ {
+		if CardHitsWithJoker[string(hand1[i])] > CardHitsWithJoker[string(hand2[i])] {
+			return true
+		} else if CardHitsWithJoker[string(hand1[i])] < CardHitsWithJoker[string(hand2[i])] {
+			return false
+		}
+	}
+
+	return false
+}
+
+func getHandTypeWithJoker(hand string) int {
+	var handType int = HIGHCARD
+
+	if isFiveOfAKindWithJoker(hand) {
+		handType = FIVEOFAKIND
+	} else if isFourOfAKindWithJoker(hand) {
+		handType = FOUROFAKIND
+	} else if isFullHouseWithJoker(hand) {
+		handType = FULLHOUSE
+	} else if isThreeOfAKindWithJoker(hand) {
+		handType = THREEOFAKIND
+	} else if isTwoPairWithJoker(hand) {
+		handType = TWOPAIR
+	} else if isOnePairWithJoker(hand) {
+		handType = ONEPAIR
+	}
+
+	return handType
+}
+
+func isFiveOfAKindWithJoker(hand string) bool {
+	var uniqueChars = make(map[string]int)
+
+	for _, card := range hand {
+		uniqueChars[string(card)]++
+	}
+
+	if len(uniqueChars) == 1 && containsJoker(uniqueChars) {
+		return true
+	}
+
+	if len(uniqueChars) == 2 && containsJoker(uniqueChars) {
+		return true
+	}
+
+	return isFiveOfAKind(hand)
+}
+
+func isFourOfAKindWithJoker(hand string) bool {
+	var uniqueChars = make(map[string]int)
+
+	for _, card := range hand {
+		uniqueChars[string(card)]++
+	}
+
+	if len(uniqueChars) == 4 && containsJoker(uniqueChars) {
+		return false
+	}
+
+	if len(uniqueChars) == 3 && containsJoker(uniqueChars) {
+		if uniqueChars["J"] == 3 {
+			return true
+		} else if uniqueChars["J"] == 2 {
+			for card, count := range uniqueChars {
+				if card != "J" && count == 1 {
+					return true
+				}
+			}
+		} else if uniqueChars["J"] == 1 {
+			for card, count := range uniqueChars {
+				if card != "J" && count == 3 {
+					return true
+				}
+			}
+		}
+	}
+
+	return isFourOfAKind(hand)
+}
+
+func isFullHouseWithJoker(hand string) bool {
+	var uniqueChars = make(map[string]int)
+
+	for _, card := range hand {
+		uniqueChars[string(card)]++
+	}
+
+	if len(uniqueChars) == 4 && containsJoker(uniqueChars) {
+		return false
+	}
+
+	if len(uniqueChars) == 3 && containsJoker(uniqueChars) {
+		if uniqueChars["J"] == 3 {
+			return true
+		} else if uniqueChars["J"] == 2 {
+			for card, count := range uniqueChars {
+				if card != "J" && count == 1 {
+					return true
+				}
+			}
+		} else if uniqueChars["J"] == 1 {
+			for card, count := range uniqueChars {
+				if card != "J" && count == 2 {
+					return true
+				}
+			}
+		}
+	}
+
+	return isFullHouse(hand)
+}
+
+func isThreeOfAKindWithJoker(hand string) bool {
+	var uniqueChars = make(map[string]int)
+
+	for _, card := range hand {
+		uniqueChars[string(card)]++
+	}
+
+	if len(uniqueChars) == 4 && containsJoker(uniqueChars) {
+		if uniqueChars["J"] == 2 {
+			return true
+		} else if uniqueChars["J"] == 1 {
+			for card, count := range uniqueChars {
+				if card != "J" && count == 2 {
+					return true
+				}
+			}
+		}
+	}
+
+	return isThreeOfAKind(hand)
+}
+
+func isTwoPairWithJoker(hand string) bool {
+	var uniqueChars = make(map[string]int)
+	var pairCount int
+
+	for _, card := range hand {
+		uniqueChars[string(card)]++
+		if uniqueChars[string(card)] == 2 {
+			pairCount++
+		}
+	}
+
+	if len(uniqueChars) == 4 && containsJoker(uniqueChars) {
+		return true
+	}
+
+	return isTwoPair(hand)
+}
+
+func isOnePairWithJoker(hand string) bool {
+	var uniqueChars = make(map[string]int)
+
+	for _, card := range hand {
+		uniqueChars[string(card)]++
+	}
+
+	if len(uniqueChars) == 5 && containsJoker(uniqueChars) {
+		return true
+	}
+
+	return isOnePair(hand)
+}
+
+func containsJoker(uniqueChars map[string]int) bool {
+	for card, _ := range uniqueChars {
+		if card == "J" {
+			return true
+		}
+	}
+
+	return false
 }
 
 func isHand1BetterThanHand2(hand1 string, hand2 string) bool {
