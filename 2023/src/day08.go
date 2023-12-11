bef2af3872c5a9f0e20b139b2a74f757e902c045
Simão Gato <simao.valedegato@gmail.com>
2023-12-11 12:19:58 +0000

    do challenge 02 day 08 aoc 2023

--- 2023/src/day08.go
+++ 2023/src/day08.go
@@ -27,6 +27,11 @@ func main() {
 	fmt.Println("Steps required to reach ZZZ are: ", steps)
 
 	// Part 2
+	initialPositions := getInitialPositionsList(network)
+
+	everySteps := getUpdatedSteps(network, instructions, initialPositions)
+
+	fmt.Println("Steps required to reach ZZZ with multiple initial positions are: ", lcmAll(everySteps[0], everySteps[1:]...))
 }
 
 func parseMapInput(scanner *bufio.Scanner) (string, map[string][]string) {
@@ -59,6 +64,18 @@ func parseMapInput(scanner *bufio.Scanner) (string, map[string][]string) {
 	return instructions, network
 }
 
+func getInitialPositionsList(network map[string][]string) []string {
+	var initialPositions []string
+
+	for key := range network {
+		if key[2] == 'A' {
+			initialPositions = append(initialPositions, key)
+		}
+	}
+
+	return initialPositions
+}
+
 func getSteps(network map[string][]string, instructions string) int {
 	steps := 0
 	currentPos := "AAA"
@@ -78,4 +95,55 @@ func getSteps(network map[string][]string, instructions string) int {
 	}
 
 	return steps
+}
+
+func getUpdatedSteps(network map[string][]string, instructions string, initialPositions []string) []int {
+	everySteps := make([]int, len(initialPositions))
+
+	for i, initialPos := range initialPositions {
+		everySteps[i] = getUpdatedStepsAux(network, instructions, initialPos)
+	}
+
+	return everySteps
+}
+
+func getUpdatedStepsAux(network map[string][]string, instructions string, initialPos string) int {
+	steps := 0
+	currentPos := initialPos
+	instructionsLen := len(instructions)
+
+	for currentPos[2] != 'Z' {
+		// find the next possible steps
+		possibleSteps := network[currentPos]
+
+		if instructions[steps%instructionsLen] == 'L' {
+			currentPos = possibleSteps[0]
+		} else {
+			currentPos = possibleSteps[1]
+		}
+
+		steps++
+	}
+
+	return steps
+}
+
+// gcd,lcm,lcmAll inspired by:
+// https://stackoverflow.com/questions/31302054/how-to-find-the-least-common-multiple-of-a-range-of-numbers
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
+func lcm(a, b int) int {
+	return a / gcd(a, b) * b
+}
+func lcmAll(a int, bs ...int) int {
+	result := a
+	for _, b := range bs {
+		result = lcm(result, b)
+	}
+
+	return result
 }
