111c9edc5ebf8aa7c803cada9cb9b0cd7a5260f3
Simão Gato <simao.valedegato@gmail.com>
2023-12-11 15:45:13 +0000

    do challenge 02 day 09 aoc 2023

--- 2023/src/day09.go
+++ 2023/src/day09.go
@@ -12,24 +12,83 @@ func main() {
 	// Parse input
 	filePath := "../inputs/day09.in"
 
+	// Day 09 - Part 1
+	extrapolatedValuesSum := calculatePosteriorExtrapolatedValuesSum(filePath)
+
+	fmt.Println("Extrapolated last values sum is: ", extrapolatedValuesSum)
+
+	// Day 09 - Part 2
+	extrapolatedValuesSum = calculateAnteriorExtrapolatedValuesSum(filePath)
+
+	fmt.Println("Extrapolated first values sum is: ", extrapolatedValuesSum)
+}
+
+func calculateAnteriorExtrapolatedValuesSum(filePath string) int {
 	file, err := os.Open(filePath)
 	if err != nil {
 		fmt.Println("Error opening the file:", err)
-		return
+		return 0
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 
-	// Day 09 - Part 1
-	extrapolatedValuesSum := calculateExtrapolatedValuesSum(scanner)
+	extrapolatedValuesSum := 0
 
-	fmt.Println("Extrapolated values sum is: ", extrapolatedValuesSum)
+	for scanner.Scan() {
+		sequence := scanner.Text()
+		extrapolatedValuesSum += calculateAnteriorSequenceValue(sequence)
+	}
 
-	// Day 09 - Part 2
+	return extrapolatedValuesSum
 }
 
-func calculateExtrapolatedValuesSum(scanner *bufio.Scanner) int {
+func calculateAnteriorSequenceValue(sequence string) int {
+	intValue := 0
+	sequenceCalculationStack := make([][]int, 1)
+
+	strValues := strings.Fields(sequence)
+
+	for _, strValue := range strValues {
+		intValue, _ = strconv.Atoi(strValue)
+		sequenceCalculationStack[0] = append(sequenceCalculationStack[0], intValue)
+	}
+
+	for !lastLineZeros(sequenceCalculationStack[len(sequenceCalculationStack)-1]) {
+		sequenceCalculationStack = append(sequenceCalculationStack, make([]int, 0))
+
+		for i := 0; i < len(sequenceCalculationStack[len(sequenceCalculationStack)-2])-1; i++ {
+			valueToPut := sequenceCalculationStack[len(sequenceCalculationStack)-2][i+1] - sequenceCalculationStack[len(sequenceCalculationStack)-2][i]
+			sequenceCalculationStack[len(sequenceCalculationStack)-1] = append(sequenceCalculationStack[len(sequenceCalculationStack)-1], valueToPut)
+		}
+	}
+
+	return calculateAnteriorValue(sequenceCalculationStack)
+}
+
+func calculateAnteriorValue(sequenceCalculationStack [][]int) int {
+	for i := 0; i < len(sequenceCalculationStack)-1; i++ {
+		idx := len(sequenceCalculationStack) - i - 1
+		minuendIdx := 0
+		subtrahendIdx := 0
+		valueToPut := sequenceCalculationStack[idx-1][minuendIdx] - sequenceCalculationStack[idx][subtrahendIdx]
+		// append to the beginning of the slice
+		sequenceCalculationStack[idx-1] = append([]int{valueToPut}, sequenceCalculationStack[idx-1]...)
+	}
+
+	return sequenceCalculationStack[0][0]
+}
+
+func calculatePosteriorExtrapolatedValuesSum(filePath string) int {
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening the file:", err)
+		return 0
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
 	extrapolatedValuesSum := 0
 
 	for scanner.Scan() {
